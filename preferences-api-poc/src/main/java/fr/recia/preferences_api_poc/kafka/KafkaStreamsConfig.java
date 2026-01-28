8343c8c7e17f405a8c49e702c603a418b1ff7c35
cailbourdin <nathan.cailbourdin@recia.fr>
2026-01-28 11:20:36 +0100

    refactor: externalisation de la conf kafka streams

--- preferences-api-poc/src/main/java/fr/recia/preferences_api_poc/kafka/KafkaStreamsConfig.java
+++ preferences-api-poc/src/main/java/fr/recia/preferences_api_poc/kafka/KafkaStreamsConfig.java
@@ -3,6 +3,7 @@ package fr.recia.preferences_api_poc.kafka;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.recia.model_kafka_poc.model.UserPreferences;
 import fr.recia.model_kafka_poc.model.UserPreferencesSerde;
+import fr.recia.preferences_api_poc.configuration.KafkaStreamProperties;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -28,7 +29,9 @@ public class KafkaStreamsConfig {
 
     public static final String STORE_NAME = "user-prefs-store";
     public static final String TOPIC_NAME = "user.preferences";
-    public static final String APPLICATION_ID = "preferences-api";
+
+    @Autowired
+    private KafkaStreamProperties kafkaStreamProperties;
 
     @Bean
     public GlobalKTable<String, UserPreferences> preferencesTable(StreamsBuilder builder, ObjectMapper objectMapper) {
@@ -44,18 +47,18 @@ public class KafkaStreamsConfig {
     @Bean(name = "defaultKafkaStreamsConfig")
     public KafkaStreamsConfiguration defaultKafkaStreamsConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092,localhost:49092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, kafkaStreamProperties.getApplicationId());
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaStreamProperties.getBootstrapServers());
         // TODO : peu importe les types qu'on met ici ça à l'air de fonctionner, c'est étrange
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
-        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 3);
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, kafkaStreamProperties.getReplicationFactor());
         // TODO : Qu'est ce qui se passe quand le router redémarre en condition de prod ?
         // -> c'est côté client que c'est stocké donc tant qu'on en a qu'un par machine donc on pourra enlever la ligne
         props.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams/preferences-api-" + UUID.randomUUID());
-        props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
-        props.put("sasl.mechanism", "PLAIN");
-        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"preferences-streams\" password=\"wJ2ngZuYh1vRZ642\";");
+        props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, kafkaStreamProperties.getSecurityProtocol());
+        props.put("sasl.mechanism", kafkaStreamProperties.getSaslMechanism());
+        props.put("sasl.jaas.config", kafkaStreamProperties.getJaasConfig());
         return new KafkaStreamsConfiguration(props);
     }
 }
--- routing-kafka-poc/src/main/java/fr/recia/routing_kafka_poc/kafka/KafkaStreamsConfig.java
+++ routing-kafka-poc/src/main/java/fr/recia/routing_kafka_poc/kafka/KafkaStreamsConfig.java
@@ -1,7 +1,9 @@
 package fr.recia.routing_kafka_poc.kafka;
 
+import fr.recia.routing_kafka_poc.configuration.KafkaStreamProperties;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -15,19 +17,22 @@ import java.util.UUID;
 @EnableKafkaStreams
 public class KafkaStreamsConfig {
 
+    @Autowired
+    private KafkaStreamProperties kafkaStreamProperties;
+
     @Bean
     public KafkaStreamsConfiguration defaultKafkaStreamsConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "basic-router");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092,localhost:49092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, kafkaStreamProperties.getApplicationId());
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaStreamProperties.getBootstrapServers());
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
-        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 3);
-        props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, kafkaStreamProperties.getReplicationFactor());
+        props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, kafkaStreamProperties.getSecurityProtocol());
         // TODO : à revoir, qu'est ce qui se passe quand le router redémarre en condition de prod ?
         props.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams/basic-router-" + UUID.randomUUID());
-        props.put("sasl.mechanism", "PLAIN");
-        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"router-streams\" password=\"VtS1g4Rru56xF6ff\";");
+        props.put("sasl.mechanism", kafkaStreamProperties.getSaslMechanism());
+        props.put("sasl.jaas.config", kafkaStreamProperties.getJaasConfig());
         return new KafkaStreamsConfiguration(props);
     }
 }
