1ab593053145cdd83b61ef583b1bf7cdb362572a
cailbourdin <nathan.cailbourdin@recia.fr>
2026-06-18 09:55:29 +0200

    chore: sleep 1s entre les build

--- scripts-bash/build.sh
+++ scripts-bash/build.sh
@@ -3,30 +3,40 @@
 cd consumer-mail
 mvn clean package &
 cd ..
+sleep 1
 cd consumer-push
 mvn clean package &
 cd ..
+sleep 1
 cd consumer-web
 mvn clean package &
 cd ..
+sleep 1
 cd producer-api
 mvn clean package &
 cd ..
+sleep 1
 cd preferences-api
 mvn clean package &
 cd ..
+sleep 1
 cd router
 mvn clean package &
 cd ..
+sleep 1
 cd expander
 mvn clean package &
 cd ..
+sleep 1
 cd service-example-kafka
 mvn clean package &
 cd ..
+sleep 1
 cd delayer
 mvn clean package -DskipTests &
 cd ..
+sleep 1
 cd smtp-proxy
 mvn clean package &
 cd ..
+sleep 1
