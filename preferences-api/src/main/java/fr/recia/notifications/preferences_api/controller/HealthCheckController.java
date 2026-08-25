23c0640b0854e1c9973e1c150057884ba88f17f9
el-miloud.tihlat <el-miloud.tihlat@recia.fr>
2026-08-25 14:33:16 +0200

    fix: ajout du code manquant au niveau des health check

--- preferences-api/src/main/java/fr/recia/notifications/preferences_api/controller/HealthCheckController.java
+++ preferences-api/src/main/java/fr/recia/notifications/preferences_api/controller/HealthCheckController.java
@@ -1,4 +1,14 @@
 package fr.recia.notifications.preferences_api.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> healthcheck() { return ResponseEntity.ok().build();
+    }
 }
