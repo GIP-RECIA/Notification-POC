68d970d406c5a4659b7ec87dbde28245085ba339
el-miloud.tihlat <el-miloud.tihlat@recia.fr>
2026-09-23 10:58:54 +0200

    feat: ajout du p12 dans le projet directement pour anticiper la mise en place de la CI

--- smtp-proxy/src/main/java/fr/recia/notifications/smtp_proxy/configuration/SMTPSslConfig.java
+++ smtp-proxy/src/main/java/fr/recia/notifications/smtp_proxy/configuration/SMTPSslConfig.java
@@ -2,20 +2,25 @@ package fr.recia.notifications.smtp_proxy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.security.KeyStore;
 
 @Configuration
 public class SMTPSslConfig {
 
     @Bean
-    public SSLContext smtpSslContext(SmtpProperties smtpProperties) throws Exception {
+    public SSLContext smtpSslContext(SmtpProperties smtpProperties, ResourceLoader resourceLoader) throws Exception {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        try (FileInputStream fis = new FileInputStream(smtpProperties.getKeystorePath())) {
-            keyStore.load(fis, smtpProperties.getKeystorePassword().toCharArray());
+        Resource resource = resourceLoader.getResource(smtpProperties.getKeystorePath());
+
+        try (InputStream fis = resource.getInputStream()) {
+            keyStore.load(fis, smtpProperties.getKeyPassword().toCharArray());
         }
 
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
